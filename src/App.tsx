1d3c6af01d9abff0ea2e511379eb39aadeb6df8b
ruchirmathur <ruchirmat@gmail.com>
2025-05-21 14:28:55 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from "react";
+
 import { Route, Routes } from "react-router-dom";
 import { AdminDashboard } from "./pages/admin";
-import { AutoLogin } from "./pages/login";
-import { AuthenticationGuard } from "./pages/authentication-guard";
 
 export const App = () => {
 
@@ -11,7 +9,7 @@ export const App = () => {
       <Route path="/" element={<AdminDashboard />} />
        <Route
         path="/admin"
-        element={<AuthenticationGuard component={<AdminDashboard />}}
+        element={<AdminDashboard />}
       />
     </Routes>
   );
